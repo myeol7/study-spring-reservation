e345ebee46188ea09d1322f36bee7d078da49701
unknown <tytekflzz@naver.com>
2026-01-09 00:05:19 +0900

    init: 스프링 부트 3.5.9 setup

---
+++ src/main/java/com/example/studyspringreservation/StudySpringReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyspringreservation/StudySpringReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
